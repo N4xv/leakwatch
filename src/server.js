911f88bbec4605e4edf1ecca96b5df7e188e6755
Astr4 <astrapch4@gmail.com>
2026-03-12 15:08:05 +0100

    Actualizar server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.post("/scan", async (req, res) => {
   const { owner, repo } = req.body;
 
   if (!owner || !repo) {
-    return res.status(400).json({ error: "owner y repo son requeridos" });
+    return res.status(400).json({ error: "owner y repo son requeridos" }); //campo obligatorio, no se debe dejar en null 
   }
 
   const findings = await scanRepo(owner, repo); //función con el enlazamiento y posterior enrutamiento de puertos en scanner.js
